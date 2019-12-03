c7f3784a8536538667c094dd0fcd28de171cb719
Brian Groenke <brian.groenke@colorado.edu>
2019-12-02 20:40:45 -0700

    Update nn2graph to use activations as weights

--- nn2graph.py
+++ nn2graph.py
@@ -27,11 +27,35 @@ def dense_activations_to_graph(model, x_in, thresh=1.0E-5):
     input_layer = model.layers[0]
     dense_layers = [l for l in model.layers if isinstance(l, Dense)]
     dense_outputs = [layer.output for layer in dense_layers]
+    # initialize keras op that includes all layer outputs
     dense_func = K.function(inputs=model.inputs, outputs=dense_outputs)
-    outputs = dense_func(x_in)
-    masks = [np.expand_dims(output > thresh, axis=1) for output in outputs]
-    Ws = [np.expand_dims(layer.get_weights()[0], axis=0) for layer in dense_layers]
+    # apply function to input
+    layer_outputs = dense_func(x_in)
+    # add axis to all outputs (including input layer)
+    outputs = [np.expand_dims(output, axis=-1) for output in [x_in] + layer_outputs]
+    # compute output masks
+    masks = [output > thresh for output in outputs]
+    # for each layer, starting with the input, broadcast the output of shape (batch, d, 1) to (batch, d, k)
+    # where k is the dimension of the next layer; i.e. repeat outputs for each node
+    Ws = [np.broadcast_to(output, (output.shape[0], output.shape[1], layer.get_weights()[0].shape[-1])) \
+          for layer, output in zip(dense_layers, outputs)]
+    # 1. apply masks to outputs
+    # 2. reshape/flatten masks from (batch, d, k) to (batch, d*k)
+    # 3. concatenate all masked outputs together to get full edge list
     Ws_masked = np.concatenate([(W*mask).reshape((-1, np.prod(W.shape[1:]))) for W, mask in zip(Ws, masks)], axis=1)
+    # create mask over final outputs for graph-tool edge filter
     edge_masks = ~np.isclose(Ws_masked, 0.0, atol=thresh)
+    # initialize graph views for all outputs in batch
     Gs = [gt.GraphView(G, efilt=G.new_ep('bool', vals=mask)) for mask in edge_masks]
+    # add degree property to graph
+    for g, acts in zip(Gs, Ws_masked):
+        g.ep['activation'] = g.new_ep('float', vals=acts)
+        g.vp['degree'] = g.degree_property_map('total', weight=g.ep['activation'])
+    # add layer labels to graph
+    layer_sizes = [x_in.shape[-1]] + [layer.units for layer in dense_layers]
+    for g in Gs:
+        labels = []
+        for i, size in enumerate(layer_sizes):
+            labels += [i]*size
+        g.vp['layer'] = g.new_vp('int', labels)
     return G, Gs
