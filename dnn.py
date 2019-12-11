68e7b39ec3b77450439e1c57ead4eaecf6662c07
mchifala <mich5456@colorado.edu>
2019-12-11 11:52:19 -0700

    Add layer by layer weights over time

--- dnn.py
+++ dnn.py
@@ -3,13 +3,12 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras import Model
 
 def build_dense(input_dims, hidden_dims=64, metrics=['acc'], optimizer='adam',
-                loss='categorical_crossentropy', reg=l2(1.0E-5)):
+                loss='categorical_crossentropy', reg=l2(1.0E-5), num_hidden_layers=4):
     input_0 = Input((input_dims,))
-    dense_1 = Dense(hidden_dims, activation='relu', kernel_regularizer=reg)(input_0)
-    dense_2 = Dense(hidden_dims, activation='relu', kernel_regularizer=reg)(dense_1)
-    dense_3 = Dense(hidden_dims, activation='relu', kernel_regularizer=reg)(dense_2)
-    dense_4 = Dense(hidden_dims, activation='relu', kernel_regularizer=reg)(dense_3)
-    dense_out = Dense(10, activation='softmax')(dense_4)
+    x = input_0
+    for i in range(num_hidden_layers):
+        x = Dense(hidden_dims, activation='relu', kernel_regularizer=reg)(x)
+    dense_out = Dense(10, activation='softmax')(x)
     model = Model(inputs=input_0, outputs=dense_out)
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     return model
